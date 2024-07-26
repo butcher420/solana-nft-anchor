dcb13724f6b9ec01af98808a1300d9279040e3dc
butcher420 <CroffieFrens251@gmail.com>
2024-07-26 08:59:15 +0700

    Update lib.rs

--- programs/solana-nft-anchor/src/lib.rs
+++ programs/solana-nft-anchor/src/lib.rs
@@ -13,7 +13,7 @@ use anchor_spl::{
 };
 use mpl_token_metadata::accounts::{ MasterEdition, Metadata as MetadataAccount };
 
-declare_id!("<UPDATE HERE>");
+declare_id!("AHfbyiQT6WxdDXhFHM2peZEPVfThHbLUSsZrw3NJxLU");
 #[program]
 pub mod solana_nft_anchor {
     use super::*;
